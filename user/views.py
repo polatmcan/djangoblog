50b10da82ba17cdc1e25ee27d175bde5dabad6e7
can <polatmuharremcan@gmail.com>
2019-08-26 21:39:42 +0300

    django blog

---
+++ user/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+# Create your views here.
+
+def register(request):
+    form=RegisterForm(request.POST or None)
+    if form.is_valid():
+        username=form.cleaned_data.get("username")
+        password=form.cleaned_data.get("password")
+                
+        newUser=User(username=username)
+        newUser.set_password(password)
+        newUser.save()
+
+        login(request,newUser)
+        messages.success(request,'Başarıyla Kayıt Oldunuz...')
+
+        return redirect("index")
+    context={
+        "form":form
+    }
+    return render(request,"register.html",context)
+
+def loginUser(request):
+    form=LoginForm(request.POST or None)
+    context={
+        "form":form
+    }
+
+    if form.is_valid():
+        username=form.cleaned_data.get("username")
+        password=form.cleaned_data.get("password")
+
+        user=authenticate(username=username,password=password)
+
+        if user is None:
+            messages.info(request,"Kullanıcı adı veya Parola hatalı.")
+            return render(request,"login.html",context)
+        messages.success(request,"Başarıyla giriş yapıldı.")
+        login(request,user)
+        return redirect("index")
+
+
+    return render(request,"login.html",context)
+def logoutUser(request):
+    logout(request)
+    messages.success(request,"Başarıyla Çıkış Yaptınız")
+    return redirect("index")
